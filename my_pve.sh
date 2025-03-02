c7a8ba121ed894cc51d8ac50831ee4cae088ee3b
dajiangfu <60643767+dajiangfu@users.noreply.github.com>
2025-03-02 17:28:40 +0800

    Add files via upload

--- my_pve.sh
+++ my_pve.sh
@@ -116,7 +116,8 @@ function install_intel_sr_iov_dkms(){
   #build-* 并不是一个官方推荐的安装方式，它可能会匹配到 大量不相关的软件
   #不推荐 直接使用 build-*，因为它可能会安装许多你 不需要的构建工具，导致系统安装冗余包
   #不建议使用 build-*，可能会安装不相关的包，占用磁盘空间并影响系统稳定性
-  apt install build-essential dkms -y
+  #apt install build-essential dkms -y
+  apt install build-essential dkms
   cd ~/i915-sriov-dkms
   if ! dkms add .; then
     red "DKMS 添加失败，退出！"
@@ -196,12 +197,12 @@ function install_intel_sr_iov_dkms(){
   #cat该文件并确保它已被修改
   cat /etc/sysfs.conf
   
-  #询问用户是否重启
   #重启 Proxmox 主机。如果使用 Proxmox 8.1 或更高版本并启用安全启动，则必须设置 MOK。
   #在 Proxmox 主机重启时，监控启动过程并等待执行 MOK 管理窗口（下面的屏幕截图）。
   #如果错过了第一次重启，则需要重新运行 mokutil 命令并再次重启。DKMS 模块将不会加载，直到您完成此设置
   #在PVE重启时的显示器启动界面依次选择Enroll MOK--->Continue--->Yes--->password(输入之前设置的MOK密码回车)--->Reboot
   #硬件里面添加PCI设备可选择虚拟出来的几个SR-IOV核显，注意要记得勾选主GPU和PCI-Express，显示设置为VirtlO-GPU，这样控制台才有画面
+  #询问用户是否重启
   read -p "已设置完毕，是否重启系统？请输入 [Y/n]: " choice
   choice=$(echo "$choice" | tr 'A-Z' 'a-z')  # 转换为小写，兼容性好，也可以用更现代的choice=${choice,,}
   [ -z "${choice}" ] && choice="y"
@@ -212,6 +213,292 @@ function install_intel_sr_iov_dkms(){
   else
     blue "已取消，请稍后自行重启。"
   fi
+}
+
+function chSensors(){
+#安装lm-sensors并配置在界面上显示
+#for i in `sed -n '/Chip drivers/,/\#----cut here/p' /tmp/sensors|sed '/Chip /d'|sed '/cut/d'`;do modprobe $i;done
+clear
+x=$(whiptail --title " PveTools   Version : 2.4.0 " --menu "配置Sensors:" 25 60 15 \
+"a" "安装配置温度、CPU频率显示" \
+"b" "删除配置" \
+3>&1 1>&2 2>&3)
+exitstatus=$?
+if [ $exitstatus = 0 ]; then
+    case "$x" in
+    a )
+        if(whiptail --title "Yes/No" --yesno "
+Your OS：$pve, you will install sensors interface, continue?(y/n)
+您的系统是：$pve, 您将安装sensors界面，是否继续？(y/n)
+            " 10 60) then
+            js='/usr/share/pve-manager/js/pvemanagerlib.js'
+            pm='/usr/share/perl5/PVE/API2/Nodes.pm'
+            sh='/usr/bin/s.sh'
+            ppv=`/usr/bin/pveversion`
+            OS=`echo $ppv|awk -F'-' 'NR==1{print $1}'`
+            ver=`echo $ppv|awk -F'/' 'NR==1{print $2}'|awk -F'-' '{print $1}'`
+            bver=`echo $ppv|awk -F'/' 'NR==1{print $2}'|awk -F'.' '{print $1}'`
+            pve=$OS$ver
+            mkdir /etc/pvetools/
+            if [ ! -f $js ];then
+                cp $js /etc/pvetools/pvemanagerlib.js
+            fi
+            if [ ! -f $pm ];then
+                cp $pm /etc/pvetools/Nodes.pm
+            fi
+            if [[ "$OS" != "pve" ]];then
+                whiptail --title "Warnning" --msgbox "
+您的系统不是Proxmox VE, 无法安装!
+                " 10 60
+                if [[ "$bver" != "5" || "$bver" != "6" || "$bver" != "7" ]];then
+                    whiptail --title "Warnning" --msgbox "
+您的系统版本无法安装!
+                    " 10 60
+                    start_menu
+                fi
+                start_menu
+            fi
+            if [[ ! -f "$js" || ! -f "$pm" ]];then
+                whiptail --title "Warnning" --msgbox "
+您的Proxmox VE版本不支持此方式！
+                " 10 60
+                start_menu
+            fi
+            #if [[ -f "$js.backup" && -f "$sh" ]];then
+            if [[ `cat $js|grep Sensors|wc -l` -gt 0 ]];then
+                whiptail --title "Warnning" --msgbox "
+您已经安装过本软件，请不要重复安装！
+                " 10 60
+                chSensors
+            fi
+            if [ ! -f "/usr/bin/sensors" ];then
+                apt-get -y install lm-sensors
+            fi
+            sensors-detect --auto > /tmp/sensors
+            drivers=`sed -n '/Chip drivers/,/\#----cut here/p' /tmp/sensors|sed '/Chip /d'|sed '/cut/d'`
+            if [ `echo $drivers|wc -w` = 0 ];then
+                whiptail --title "Warnning" --msgbox "
+没有找到任何驱动，似乎你的系统没有温度传感器。
+继续配置CPU频率...
+                " 10 60
+                if [ $bver -gt 7 ];then
+                    cat << EOF > /usr/bin/s.sh
+curC=\`cat /proc/cpuinfo|grep MHz|awk 'NR==1{print \$4}'\`
+max=\`cat /proc/cpuinfo|grep GHz|awk -F "@" 'NR==1{print \$2}'|sed 's/GHz//g'|sed 's/\ //g'\`
+maxC=\`echo "\$max * 1000"|bc -l\`
+minC=\`lscpu|grep 'min MHz'|awk '{print \$4}'\`
+c="\"CPU-MHz\":\""\$curC"\",\"CPU-max-MHz\":\""\$maxC"\",\"CPU-min-MHz\":\""\$minC"\""
+r="{"\$c"}"
+echo \$r
+EOF
+                else
+                    cat << EOF > /usr/bin/s.sh
+c=\`lscpu|grep MHz|sed 's/CPU\ /CPU-/g'|sed 's/\ MHz/-MHz/g'|sed 's/\ //g'|sed 's/^/"/g'|sed 's/$/"\,/g'|sed 's/\:/\"\:\"/g'|awk 'BEGIN{ORS=""}{print \$0}'|sed 's/\,\$//g'\`
+r="{"\$c"}"
+echo \$r
+EOF
+                fi
+            chmod +x /usr/bin/s.sh
+            #--create the configs--
+            if [ -f ./p1 ];then rm ./p1;fi
+            #--这里插入cpu频率--
+            cat << EOF >> ./p1
+             ,{
+             itemId: 'MHz',
+             colspan: 2,
+             printBar: false,
+             title: gettext('CPU频率'),
+             textField: 'tdata',
+             renderer:function(value){
+                 var d = JSON.parse(value);
+                 f0 = d['CPU-MHz'];
+                 f1 = d['CPU-min-MHz'];
+                 f2 = d['CPU-max-MHz'];
+                 return  \`CPU实时(Cur): \${f0} MHz | 最小(min): \${f1} MHz | 最大(max): \${f2} MHz \`;
+         }
+ }
+EOF
+            #--插入cpu频率结束--
+            cat << EOF >> ./p2
+\$res->{tdata} = \`/usr/bin/s.sh\`;
+EOF
+            n=`sed '/pveversion/,/\}/=' $js -n|sed -n '$p'`
+            sed -i ''$n' r ./p1' $js
+            n=`sed '/pveversion/,/version_text/=' $pm -n|sed -n '$p'`
+            sed -i ''$n' r ./p2' $pm
+            if [ -f ./p1 ];then rm ./p1;fi
+            if [ -f ./p2 ];then rm ./p2;fi
+            systemctl restart pveproxy
+            whiptail --title "Success" --msgbox "
+如果没有意外，已经安装完成！浏览器打开界面刷新看一下概要界面！
+            " 10 60
+
+                chSensors
+            else
+                for i in $drivers
+                do
+                    modprobe $i
+                    if [ `grep $i /etc/modules|wc -l` = 0 ];then
+                        echo $i >> /etc/modules
+                    fi
+                done
+                sensors
+                sleep 3
+                whiptail --title "Success" --msgbox "
+安装配置成功，如果没有意外，上面已经显示sensors。下一步会重启web界面，请不要惊慌。
+                " 20 60
+            rm /tmp/sensors
+            #debian 12 fixbug
+            if [ $bver -gt 7 ];then
+                cat << EOF > /usr/bin/s.sh
+r=\`sensors|grep -E 'Package id 0|fan|Physical id 0|Core'|grep '^[a-zA-Z0-9].[[:print:]]*:.\s*\S*[0-9].\s*[A-Z].' -o|sed 's/:\ */:/g'|sed 's/:/":"/g'|sed 's/^/"/g' |sed 's/$/",/g'|sed 's/\ C\ /C/g'|sed 's/\ V\ /V/g'|sed 's/\ RP/RPM/g'|sed 's/\ //g'|awk 'BEGIN{ORS=""}{print \$0}'|sed 's/\,\$//g'|sed 's/°C/\&degC/g'\`
+curC=\`cat /proc/cpuinfo|grep MHz|awk 'NR==1{print \$4}'\`
+max=\`cat /proc/cpuinfo|grep GHz|awk -F "@" 'NR==1{print \$2}'|sed 's/GHz//g'|sed 's/\ //g'\`
+maxC=\`echo "\$max * 1000"|bc -l\`
+minC=\`lscpu|grep 'min MHz'|awk '{print \$4}'\`
+c="\"CPU-MHz\":\""\$curC"\",\"CPU-max-MHz\":\""\$maxC"\",\"CPU-min-MHz\":\""\$minC"\""
+r="{"\$r","\$c"}"
+echo \$r
+EOF
+            else
+                cat << EOF > /usr/bin/s.sh
+r=\`sensors|grep -E 'Package id 0|fan|Physical id 0|Core'|grep '^[a-zA-Z0-9].[[:print:]]*:.\s*\S*[0-9].\s*[A-Z].' -o|sed 's/:\ */:/g'|sed 's/:/":"/g'|sed 's/^/"/g' |sed 's/$/",/g'|sed 's/\ C\ /C/g'|sed 's/\ V\ /V/g'|sed 's/\ RP/RPM/g'|sed 's/\ //g'|awk 'BEGIN{ORS=""}{print \$0}'|sed 's/\,\$//g'|sed 's/°C/\&degC/g'\`
+c=\`lscpu|grep MHz|sed 's/CPU\ /CPU-/g'|sed 's/\ MHz/-MHz/g'|sed 's/\ //g'|sed 's/^/"/g'|sed 's/$/"\,/g'|sed 's/\:/\"\:\"/g'|awk 'BEGIN{ORS=""}{print \$0}'|sed 's/\,\$//g'\`
+r="{"\$r","\$c"}"
+echo \$r
+EOF
+            fi
+            chmod +x /usr/bin/s.sh
+            #--create the configs--
+            #--filter for sensors 过滤sensors项目--
+            d=`sensors|grep -E 'Package id 0|fan|Physical id 0|Core'|grep '^[a-zA-Z0-9].[[:print:]]*:.\s*\S*[0-9].\s*[A-Z].' -o|sed 's/:\ */:/g'|sed 's/\ C\ /C/g'|sed 's/\ V\ /V/g'|sed 's/\ RP/RPM/g'|sed 's/\ //g'|awk -F ":" '{print $1}'`
+            if [ -f ./p1 ];then rm ./p1;fi
+            #--这里插入cpu频率--
+            cat << EOF >> ./p1
+             ,{
+             itemId: 'MHz',
+             colspan: 2,
+             printBar: false,
+             title: gettext('CPU频率'),
+             textField: 'tdata',
+             renderer:function(value){
+                 var d = JSON.parse(value);
+                 f0 = d['CPU-MHz'];
+                 f1 = d['CPU-min-MHz'];
+                 f2 = d['CPU-max-MHz'];
+                 return  \`CPU实时(Cur): \${f0} MHz | 最小(min): \${f1} MHz | 最大(max): \${f2} MHz \`;
+         }
+ }
+EOF
+            #--插入cpu频率结束--
+            cat << EOF >> ./p1
+        ,{
+            xtype: 'box',
+            colspan: 2,
+        title: gettext('Sensors Data:'),
+            padding: '0 0 20 0'
+        }
+        ,{
+            itemId: 'Sensors',
+            colspan: 2,
+            printBar: false,
+            title: gettext('Sensors Data:')
+        }
+EOF
+            for i in $d
+            do
+            cat << EOF >> ./p1
+        ,{
+            itemId: '$i',
+            colspan: 1,
+            printBar: false,
+            title: gettext('$i'),
+            textField: 'tdata',
+            renderer:function(value){
+            var d = JSON.parse(value);
+            var s = "";
+            s = d['$i'];
+            return s;
+            }
+        }
+EOF
+            done
+            cat << EOF >> ./p2
+\$res->{tdata} = \`/usr/bin/s.sh\`;
+EOF
+#\$res->{cpusensors} = \`lscpu | grep MHz\`;
+            #--configs end--
+            #h=`sensors|awk 'END{print NR}'`
+            itemC=`s.sh|sed  's/\,/\r\n/g'|wc -l`
+            if [ $itemC = 0 ];then
+                h=400
+            else
+                #let h=$h*9+320
+                let h=$itemC*24/2+360
+            fi
+            n=`sed '/widget.pveNodeStatus/,/height/=' $js -n|sed -n '$p'`
+            sed -i ''$n'c \ \ \ \ height:\ '$h',' $js
+            n=`sed '/pveversion/,/\}/=' $js -n|sed -n '$p'`
+            sed -i ''$n' r ./p1' $js
+            n=`sed '/pveversion/,/version_text/=' $pm -n|sed -n '$p'`
+            sed -i ''$n' r ./p2' $pm
+            if [ -f ./p1 ];then rm ./p1;fi
+            if [ -f ./p2 ];then rm ./p2;fi
+            systemctl restart pveproxy
+            whiptail --title "Success" --msgbox "
+如果没有意外，已经安装完成！浏览器打开界面刷新看一下概要界面！
+            " 10 60
+        fi
+        else
+            chSensors
+        fi
+    ;;
+    b )
+        if(whiptail --title "Yes/No" --yesno "
+确认要还原配置？
+        " 10 60)then
+            js='/usr/share/pve-manager/js/pvemanagerlib.js'
+            pm='/usr/share/perl5/PVE/API2/Nodes.pm'
+
+            if [[ `cat $js|grep -E 'Sensors|CPU'|wc -l` = 0 ]];then
+                whiptail --title "Warnning" --msgbox "
+没有检测到安装，不需要卸载。
+                " 10 60
+            else
+                sensors-detect --auto > /tmp/sensors
+                drivers=`sed -n '/Chip drivers/,/\#----cut here/p' /tmp/sensors|sed '/Chip /d'|sed '/cut/d'`
+                if [ `echo $drivers|wc -w` != 0 ];then
+                    for i in $drivers
+                    do
+                        if [ `grep $i /etc/modules|wc -l` != 0 ];then
+                            sed -i '/'$i'/d' /etc/modules
+                        fi
+                    done
+                fi
+                apt-get -y remove lm-sensors
+            {
+                #mv $js.backup $js
+                #mv $pm.backup $pm
+                #rm $js
+                #rm $pm
+                rm /usr/bin/s.sh
+                #cp /etc/pvetools/pvemanagerlib.js $js
+                #cp /etc/pvetools/Nodes.pm $pm
+                apt install --reinstall pve-manager
+                systemctl restart pveproxy
+                echo 50
+                echo 100
+                sleep 1
+            }|whiptail --gauge "Uninstalling" 10 60 0
+            whiptail --title "Success" --msgbox "
+卸载成功。
+            " 10 60
+            fi
+        fi
+        chSensors
+        ;;
+    esac
+fi
 }
 
 #开始菜单
@@ -229,6 +516,7 @@ start_menu(){
   green " 4. 取消无效订阅弹窗"
   green " 5. PVE软件源更换"
   green " 6. 开启intel核显SR-IOV虚拟化直通"
+  green " 7. 配置pve的web界面显示传感器温度、CPU频率"
   blue " 0. 退出脚本"
   echo
   read -p "请输入数字:" num
@@ -265,6 +553,12 @@ start_menu(){
   ;;
   6)
   install_intel_sr_iov_dkms
+  sleep 1s
+  read -s -n1 -p "按任意键返回上级菜单 ... "
+  start_menu
+  ;;
+  7)
+  chSensors
   sleep 1s
   read -s -n1 -p "按任意键返回上级菜单 ... "
   start_menu
